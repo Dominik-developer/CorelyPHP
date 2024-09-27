689a4887cbb65e6d8966bdf56b938a4aab4f5c62
Dominik Szczepański <163560221+Dominik-developer@users.noreply.github.com>
2024-09-27 23:17:07 +0200

    Add files via upload

--- public/single.php
+++ public/single.php
@@ -3,17 +3,21 @@
 
 session_start();
 
-include_once 'page.functions.php';
+include_once 'functions.php';
 require_once 'service.alg.php';
+include_once 'articles.alg.php';
 
 service();
+
+
+$restored_title = str_replace('_', ' ', $_GET['title']); //optionally use - insted of _
 
 ?>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Blog page: <?php ?></title>
+    <title><?php echo $restored_title; ?></title>
 
     <link rel="stylesheet" href="main.css" />
 
@@ -26,18 +30,20 @@ service();
     </header>
 
     <nav>
-        <a class="current"><?php ?></a>
+        <a href="main.php">Home Page</a> 
+        <!--<a>===></a>-->
+        <a class="current"><?php echo $restored_title; ?></a>
     </nav>
 
     <main>
         <section class="main">
             <?php
 
-                echo 'article will be here in future';
-                echo '<br>';
-
-                echo $_GET['id'];
-
+                //echo 'article will be here in future';
+                //echo '<br>';
+                //echo $_GET['title'];
+                //echo $restored_title;
+                articles($restored_title);
 
             ?>
         </section>
