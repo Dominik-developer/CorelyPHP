bd57a37afb545bb53b4757576f442b52d4ac4398
Dominik <163560221+Dominik-developer@users.noreply.github.com>
2025-01-19 11:16:59 +0100

    aditarticle() update

--- admin/window_functions.php
+++ admin/window_functions.php
@@ -102,90 +102,86 @@ if(!isset ($_SESSION['adminLoged']))
 
     // edit article ==========================================
 
+  
     function editArticle(){
-            
+                
         if (isset($_GET['id']) && ctype_digit($_GET['id']) && (int)$_GET['id'] > 0) {
         
         require_once 'data_loading.alg.php';
 
         $articleData = edit_dataLoading($_GET['id']);
 
-        if ($articleData) {
-            //echo "Content: " . $articleData['photo_path'] . "<br>";
-
-        } else {
-            echo "No article data found.";
-        }
-
+            if ($articleData === null) {
+                
+                echo "No article data found.";
+            } else {
     ?>
-        <article class="window">
-            <section class="main" id="edit">
-                <h2>Edit article <?php echo "ID: " . $articleData['ID']; ?></h2>
-                <section class="content">
-                    <form action="./algo/edit.alg.php" method="POST" enctype="multipart/form-data">
-                        <div class="row">
-                            <div class="col-25">
-                                <label for="title">Article title</label>
+            <article class="window">
+                <section class="main" id="edit">
+                    <h2>Edit article <?php echo "ID: " . $articleData['ID']; ?></h2>
+                    <section class="content">
+                        <form action="./algo/edit.alg.php" method="POST" enctype="multipart/form-data">
+                            <div class="row">
+                                <div class="col-25">
+                                    <label for="title">Article title</label>
+                                </div>
+                                <div class="col-75">
+                                    <input type="text" id="title" name="title" required="required" placeholder="Title.." patter="" value="<?php echo $articleData['title']; ?>">
+                                </div>
                             </div>
-                            <div class="col-75">
-                                <input type="text" id="title" name="title" required="required" placeholder="Title.." patter="" value="<?php echo $articleData['title']; ?>">
+                            <div class="row">
+                                <div class="col-25">
+                                    <label for="textUpload">Article text</label>
+                                </div>
+                                <div class="col-75">
+                                    <textarea id="articleContent" name="articleContent" required="required" placeholder="Write your article content here..." rows="10" cols="50"><?php  echo $articleData['text']; ?></textarea>
+                                </div>
                             </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-25">
-                                <label for="textUpload">Article text</label>
+                                <div class="col-25">
+                                    <label for="photoUpload">See old photo</label>
+                                </div>
+                                <div class="col-75">
+                                    <img src="<?php echo $articleData['photo_path']; ?>" alt="<?php echo $articleData['title']; ?> photo" width="150" height="120">
+                                </div>
                             </div>
-                            <div class="col-75">
-
-                            <textarea id="articleContent" name="articleContent" required="required" placeholder="Write your article content here..." rows="10" cols="50"><?php  echo $articleData['text']; ?></textarea>
-                            <!--<input type="file" id="textUpload" name="textUpload" required="required" accept=".txt, .doc, .docx" enctype="multipart/form-data">-->
+                            <div class="row">
+                                <div class="col-25">
+                                    <label for="photoUpload">Submit new photo</label>
+                                </div>
+                                <div class="col-75">
+                                    <input type="file" id="photoUpload" name="photoUpload" required="required" accept="image/png, image/jpeg" enctype="multipart/form-data">
+                                </div>
                             </div>
-                        </div>
-                        <!--<div class="row">
-                            <div class="col-25">
-                                <label for="photoUpload">See old photo</label>
+                                <br>
+                            <div class="row align-checkbox">
+                                <input type="checkbox" id="visibility-checkbox" name="visibility" required="required">
+                                <label for="visibility-checkbox">I confirm the changes to the article.</label>
                             </div>
-                            <div class="col-75">
-                                <a class="custom-button blue" id="showBttn" onclick="myFunction()">See</a>
+                                <br>
+                            <div class="row align-checkbox">
+                                <input type="submit" value="Submit">
+                                <!--<button class="blue" type="submit" name="submit">Submit</button>--> <!-- if works, change for this one -->
                             </div>
-                        </div>-->
-                        <div class="row">
-                            <div class="col-25">
-                                <label for="photoUpload">Submit new photo</label>
-                            </div>
-                            <div class="col-75">
-                                <input type="file" id="photoUpload" name="photoUpload" required="required" accept="image/png, image/jpeg" enctype="multipart/form-data">
-                            </div>
-                        </div>
+                        </form>
                             <br>
-                        <div class="row align-checkbox">
-                            <input type="checkbox" id="visibility-checkbox" name="visibility" required="required">
-                            <label for="visibility-checkbox">I confirm the changes to the article.</label>
-                        </div>
-                            <br>
-                        <div class="row">
-                            <input type="submit" value="Submit">
-                        </div>
-                    </form>
-                        <br>
-                   <!--<form action="./algo/delete.alg.php" method="POST">
+                        <form action="./algo/delete.alg.php" method="POST">
                             <div class="row align-checkbox">
                                 <input type="checkbox" id="passwordChange-checkbox" name="passwordChange" required="required">
                                 <label for="passwordChange-checkbox">I confirm delete of article.</label>
                             </div>
-                                <br>
+                                    <br>
                             <div class="row">
                                 <button class="red" type="submit" name="send">Delete article</button>
                             </div>
-                        </form>-->
+                        </form>
+                    </section>
                 </section>
-            </section>
-        </article>
-        
+            </article>
     <?php
-    
+            }
+
         } else {
-    
+
     ?>
         <article class="window">
             <section class="main" id="all">
@@ -201,7 +197,7 @@ if(!isset ($_SESSION['adminLoged']))
             </section>
         </article>
     <?php
-    
+
         }
     }
     
