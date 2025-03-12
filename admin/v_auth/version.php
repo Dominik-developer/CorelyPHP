bc99f220356356b4c3e15c805be7c3d0388479f2
Dominik <163560221+Dominik-developer@users.noreply.github.com>
2025-03-12 18:09:46 +0100

    version control for: PHP

---
+++ admin/v_auth/version.php
@@ -0,0 +1,11 @@
+<?php
+// CorelyPHP
+define('APP_NAME', 'CorelyPHP');
+define('APP_VERSION', '1.1.0');
+define('RELEASE_DATE', '2025-02-XX');
+
+// PHP
+define('PHP_VERSION_REQ', '8.2.0');
+define('PHP_VERSION_ADVICE', 'BEST: 8.3.0+, OPTIMALLY 8.0+ OR AT LEAST 7.0.0 (NOT RECOMMENDED)');
+
+
